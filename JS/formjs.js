5bd9ddeee697d91403e6c1362522768ebdc80801
tushar.kasturi_ymedi <tusharkasturi1999@gmail.com>
2021-09-30 20:32:12 +0530

    Added validateEmail

--- JS/formjs.js
+++ JS/formjs.js
@@ -1,10 +1,20 @@
+//name validation
 const textError = document.querySelector(".text-error");
 text.oninput = function () {
   let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
   if (nameRegex.test(text.value)) textError.textContent = "";
   else textError.textContent = "Name is Incorrect";
 };
-
+//email validation
+const emailError = document.querySelector(".email-error");
+email.oninput = function () {
+  let emailRegex = RegExp(
+    "^[a-zA-Z0-9-_+]+(\\.?[a-zA-Z0-9-_]+)@[a-zA-Z0-9-_]+\\.[a-zA-Z]{2,}(\\.?[a-zA-Z-_]+)$"
+  );
+  if (emailRegex.test(email.value)) emailError.textContent = "";
+  else emailError.textContent = "Email is Incorrect";
+};
+//salary validation
 const salary = document.querySelector("#salary");
 const output = document.querySelector(".salary-output");
 output.textContent = salary.value;
